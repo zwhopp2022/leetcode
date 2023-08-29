3a73f2048770c3bc9cefa2c9c96d31959b2fcc5b
Zachary <zacharyhopp01@gmail.com>
2023-08-29 11:54:13 -0400

    Completed Running Sum of 1D Array

---
+++ src/runningSum.java
@@ -0,0 +1,30 @@
+// 1480. Running Sum of 1D Array
+// Completed by Zachary Hopp 8/29/2023
+public class runningSum {
+    public static int[] calcRunningSum(int[] nums) {
+        int[] results = new int[nums.length];
+
+        results[0] = nums[0];
+
+        for (int index = 1; index < nums.length; index++) {
+            results[index] = nums[index] + results[index - 1];
+        }
+
+        return results;
+    }
+
+    // time complexity = O(n)
+    // space complexity = O(1)
+}
+
+class Main {
+    public static void main(String[] args) {
+        int[] enter = new int[]{3, 0, 1, 2};
+
+        int[] results = runningSum.calcRunningSum(enter);
+
+        for (int num : results) {
+            System.out.println("Running Sum: " + num);
+        }
+    }
+}
